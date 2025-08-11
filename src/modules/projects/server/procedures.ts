b4925d85189136aee50c1b6dbf113ee67f8ab3ba
Rajat Gangwar <rajat.gangwar41@gmail.com>
2025-08-11 23:44:58 +0530

    messsage ui part added

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -3,8 +3,27 @@ import { inngest } from "@/inngest/client";
 import prisma from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { generateSlug } from "random-word-slugs";
+import { TRPCError } from "@trpc/server";
 
 export const projectsRouter = createTRPCRouter({
+  getOne: baseProcedure
+    .input(z.object({ id: z.string().min(1, { message: "Id is required" }) }))
+    .query(async ({ input }) => {
+      const existingProject = await prisma.project.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!existingProject) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Project not found",
+        });
+      }
+
+      return existingProject;
+    }),
   getMany: baseProcedure.query(async () => {
     const projects = await prisma.project.findMany({
       orderBy: {
